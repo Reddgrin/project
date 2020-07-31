43897f193f7bd40b0b53148a3501a043569d5b8d
Vadim <example@gmail.com>
2020-07-31 20:38:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log('arr'+ "- object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(++incr);
+console.log(--decr);
+
+const isChecked = true,
+      isClose = true; 
+
+      console.log(isChecked || isClose);
